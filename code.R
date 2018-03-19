f02312a705178814c3bef75d50390d0c62655877
Danielle Quinn <danielle.quinn@acadiau.ca>
2018-03-19 19:39:46 -0230

    Added d1d4 option and tidied sections

--- code.R
+++ code.R
@@ -1,12 +1,11 @@
-# Load Workspace
+# ---- Load Workspace and Packages ----
 load("data/ISDB.ISTRIPS.RData")
 load("data/ISDB.ISFISHSETS.RData")
 load("data/ISDB.ISSETPROFILE_WIDE.RData")
-
-# Load Packages
 library(dplyr)
 library(lubridate)
 
+# ---- Data Manipulation ----
 # Use left join to add TRIP_ID to ISSETPROFILE_WIDE
 ISSETPROFILE_WIDE<-left_join(ISSETPROFILE_WIDE, ISFISHSETS%>%select(FISHSET_ID, TRIP_ID))
 ISSETPROFILE_WIDE$DATE_TIME1[year(ISSETPROFILE_WIDE$DATE_TIME1)==9999]<-NA
@@ -14,6 +13,7 @@ ISSETPROFILE_WIDE$DATE_TIME2[year(ISSETPROFILE_WIDE$DATE_TIME2)==9999]<-NA
 ISSETPROFILE_WIDE$DATE_TIME3[year(ISSETPROFILE_WIDE$DATE_TIME3)==9999]<-NA
 ISSETPROFILE_WIDE$DATE_TIME4[year(ISSETPROFILE_WIDE$DATE_TIME4)==9999]<-NA
 
+# ---- Create date_ref ----
 # Set up table describing datetime structure to reduce wasted time in loop
 date_ref<-ISSETPROFILE_WIDE%>%
   group_by(TRIP_ID)%>%
@@ -24,8 +24,8 @@ date_ref<-ISSETPROFILE_WIDE%>%
 
 # ---- Type 1: Available d2 & d3 ----
 trips_23<-(date_ref%>%
-  filter(d1==0 & d4==0 & d2>0 & d3>0)%>%
-  select(TRIP_ID)%>%data.frame())$TRIP_ID
+             filter(d1==0 & d4==0 & d2>0 & d3>0)%>%
+             select(TRIP_ID)%>%data.frame())$TRIP_ID
 
 # Set up error reports
 error_type1<-c() # if datetime2 < datetime3
@@ -74,6 +74,64 @@ error1_details
 error_type2
 error2_details
 
+
+
+
+
+
+
+
+# ---- Type 2: Available d1 & d4 ----
+# Create function used for both 23 and 14 structures
+trips_14<-(date_ref%>%
+             filter(d1>0 & d4>0 & d2==0 & d3==0)%>%
+             select(TRIP_ID)%>%data.frame())$TRIP_ID
+
+# Set up error reports
+error_type1<-c() # if datetime2 < datetime3
+error_type2<-c() # if set n < set n-1
+error1_details<-list()
+error2_details<-list()
+
+# Optional code for estimating efficiency
+total<-0 # count total trips considered
+starttime<-now()
+
+# Check for errors and generate error reports
+for(i in unique(trips_14)[1:1000])
+{
+  total=total+1
+  testdata<-ISSETPROFILE_WIDE%>%filter(TRIP_ID==i)
+  testdata<-testdata[order(testdata$SET_NO),]
+  
+  # Step 1: Are all datetime1 > datetime4?
+  if(length(which(testdata$DATE_TIME4<testdata$DATE_TIME1))>0)
+  {
+    error_type1<-c(error_type1,i)
+    error1_details<-append(error1_details, list(c(testdata$SET_NO[which(testdata$DATE_TIME4<testdata$DATE_TIME1)])))
+    names(error1_details)[length(error1_details)]<-i
+  }
+  
+  # Step 2: Are all datetime1 > datetime4-1
+  if(length(which(testdata$DATE_TIME1[-1]<testdata$DATE_TIME4[1:nrow(testdata)-1]))>0)
+  {
+    error_type2<-c(error_type2, i)
+    error2_details<-append(error2_details, list(c(testdata$SET_NO[which(testdata$DATE_TIME1[-1]<testdata$DATE_TIME4[1:nrow(testdata)-1])])))
+    names(error2_details)[length(error2_details)]<-i
+  }
+}
+
+# Optional code for estimating efficiency
+endtime<-now()
+paste("Time elapsed:", round(difftime(endtime, starttime, unit='sec'),2), "seconds")
+paste("Checks per time:", round(total/as.numeric(difftime(endtime, starttime, unit='sec')),2), "checks per second")
+paste("Time per check:", round(as.numeric(difftime(endtime, starttime, unit='sec'))/total,2), "seconds per check")
+
+# View error reports
+error_type1
+error1_details
+error_type2
+error2_details
 
 
 
