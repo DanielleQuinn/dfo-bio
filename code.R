b68bd3f7528f53a3527c67f8e8cabb0c3decfd20
Danielle Quinn <danielle.quinn@acadiau.ca>
2018-03-19 20:12:55 -0230

    Added pieces for no datetime and for all datetime.

--- code.R
+++ code.R
@@ -133,6 +133,69 @@ error1_details
 error_type2
 error2_details
 
+# ---- Type 4: Available d1, d2, d3, d4 ----
+trips_1234<-(date_ref%>%
+            filter(d1>0 & d4>0 & d2>0 & d3>0)%>%
+            select(TRIP_ID)%>%data.frame())$TRIP_ID
+
+# Set up error reports
+error_type1<-c() # if datetime2 < datetime3
+error_type2<-c() # if set n < set n-1
+error1_details<-list()
+error2_details<-list()
+
+# Optional code for estimating efficiency
+total<-0 # count total trips considered
+starttime<-now()
+
+# Check for errors and generate error reports
+for(i in unique(trips_1234)[1:10])
+{
+  total=total+1
+  testdata<-ISSETPROFILE_WIDE%>%filter(TRIP_ID==i)
+  testdata<-testdata[order(testdata$SET_NO),]
+  
+  # Step 1: Are all datetime1 > datetime2 > datetime3 > datetime4?
+  if(length(which(testdata$DATE_TIME2<testdata$DATE_TIME1 |
+                  testdata$DATE_TIME3<testdata$DATE_TIME2 |
+                  testdata$DATE_TIME4<testdata$DATE_TIME3))>0)
+  {
+    error_type1<-c(error_type1,i)
+    error1_details<-append(error1_details, list(c(testdata$SET_NO[which(testdata$DATE_TIME2<testdata$DATE_TIME1 |
+                                                                          testdata$DATE_TIME3<testdata$DATE_TIME2 |
+                                                                          testdata$DATE_TIME4<testdata$DATE_TIME3)])))
+    names(error1_details)[length(error1_details)]<-i
+  }
+  
+  # Step 2: Are all datetime1 > datetime4-1
+  if(length(which(testdata$DATE_TIME1[-1]<testdata$DATE_TIME4[1:nrow(testdata)-1]))>0)
+  {
+    error_type2<-c(error_type2, i)
+    error2_details<-append(error2_details, list(c(testdata$SET_NO[which(testdata$DATE_TIME1[-1]<testdata$DATE_TIME4[1:nrow(testdata)-1])])))
+    names(error2_details)[length(error2_details)]<-i
+  }
+}
+
+# Optional code for estimating efficiency
+endtime<-now()
+paste("Time elapsed:", round(difftime(endtime, starttime, unit='sec'),2), "seconds")
+paste("Checks per time:", round(total/as.numeric(difftime(endtime, starttime, unit='sec')),2), "checks per second")
+paste("Time per check:", round(as.numeric(difftime(endtime, starttime, unit='sec'))/total,2), "seconds per check")
+
+# View error reports
+error_type1
+error1_details
+error_type2
+error2_details
+
+# test: yes, this is accurate
+ISSETPROFILE_WIDE%>%filter(TRIP_ID==8802)%>%
+  select(SET_NO, DATE_TIME1, DATE_TIME2, DATE_TIME3, DATE_TIME4)
+
+# ---- Type 3: No Available datetime information ----
+trips_0<-(date_ref%>%
+             filter(d1==0 & d4==0 & d2==0 & d3==0)%>%
+             select(TRIP_ID)%>%data.frame())$TRIP_ID
 
 
 
